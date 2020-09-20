98a1950f9e1e0a2aee3a8374e25c085eda4c1c24
yanjs <felixyjs@gmail.com>
2020-09-20 09:41:08 -0400

    It works now

--- healthMIT/app/routes.py
+++ healthMIT/app/routes.py
@@ -2,12 +2,39 @@ from flask import render_template
 from flask import request
 from app import app
 
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/index', methods=['POST', 'GET'])
+import sys
+import os
+import dlib
+import glob
+
+predictor_path = 'app/nose.dat'
+
+def has_nose(imgfile):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(predictor_path)
+    img = dlib.load_rgb_image(imgfile)
+    dets = detector(img, 1)
+
+    print("Number of noses detected: {}".format(len(dets)))
+    if dets:
+        return True
+    else:
+        return False
+
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', result='No')
-    elif request.method == 'POST':
-        data = request.form
-        return render_template('index.html', result='Yes')
-    return None
+    return render_template('index.html', result='Please Upload an image with masked face')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    img = request.files['imgfile']
+    name=img.filename
+    img.save(name)
+    nose = has_nose(name)
+    if nose:
+        prompt = 'We detect a nose in this image. Maybe your mask did not cover your nose.'
+    else:
+        prompt = 'We did\'t detect a nose in this image. Maybe your mask covered your nose.'
+
+    return render_template('index.html', result=prompt)
+
