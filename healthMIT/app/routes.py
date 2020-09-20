0f5a1d368edf33c018bbacf8dc633da35c65008c
yanjs <felixyjs@gmail.com>
2020-09-20 08:53:42 -0400

    Add upload button

--- healthMIT/app/routes.py
+++ healthMIT/app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
+from flask import request
 from app import app
-@app.route('/')
-@app.route('/index')
+
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    if request.method == 'GET':
+        return render_template('index.html', result='No')
+    elif request.method == 'POST':
+        data = request.form
+        return render_template('index.html', result='Yes')
+    return None
