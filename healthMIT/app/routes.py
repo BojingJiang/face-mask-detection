897c084aad0c2c4b4b396f6e49633d134e8f872a
BojingJiang <40879743+BojingJiang@users.noreply.github.com>
2022-08-31 17:44:41 -0400

    Update routes.py

--- healthMIT/app/routes.py
+++ healthMIT/app/routes.py
@@ -9,13 +9,13 @@ import glob
 
 predictor_path = 'app/nose.dat'
 
-def has_nose(imgfile):
+def nose_detected(imgfile):
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(predictor_path)
     img = dlib.load_rgb_image(imgfile)
     dets = detector(img, 1)
 
-    print("Number of noses detected: {}".format(len(dets)))
+    print("# nose detected: {}".format(len(dets)))
     if dets:
         return True
     else:
@@ -23,7 +23,7 @@ def has_nose(imgfile):
 
 @app.route('/')
 def index():
-    return render_template('index.html', result='Please Upload an image with masked face')
+    return render_template('index.html', result='Please Upload an image with a masked face')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -31,7 +31,7 @@ def upload():
     dotname = img.filename.split('.')[-1]
     name='temp.' + dotname
     img.save(name)
-    nose = has_nose(name)
+    nose = nose_detected(name)
     if nose:
         prompt = 'We have detected an uncovered nose in this image.'
     else:
